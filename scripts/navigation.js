be1734bbcdce1ff6d20c545ecccd766c53e95865
Mike Sinkula <mike@mikesinkula.com>
2017-03-20 18:58:27 -0700

    more edits to sub navigation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -7,40 +7,40 @@ Author URI: http://www.premiumdw.com/
 
 $(window).load(function() { // when the window loads...
     
-	$('#toggle').click(function(e) { // when the hamburger is clicked...  
-        
-		$('#navigation').slideToggle(); // ... toggle main-menu
-        
-		e.preventDefault(); // disable href on main-menu item
-        
-	});
-    
+    $('#toggle').click(function(e) { // when the hamburger is clicked...  
+
+        $('#navigation').slideToggle(); // ... toggle main-menu
+
+        e.preventDefault(); // disable href on main-menu item
+
+    });
+
     $('li.menu-item-has-children > a').after('<span class="sub-toggle">&#711;</span>'); // create the sub-toggle arrow
-    
+
     $('span.sub-toggle').click(function() { // when the arrow is clicked...
-        
+
         $(this).next('ul.sub-menu').slideToggle(); // ... toggle it's sub-menu
-                
+
     });
 	
-	var loadWidth = window.innerWidth; // save window load width as a variable
-	
-	$(window).resize(function() { // when the window is resized...
-			
-		if ( loadWidth !== window.innerWidth ) { // trigger for width only...
-			
-			if (window.innerWidth < 801) { // if width is less than 801px...
-			
-				$('#navigation').hide(); // hide main navigation items
+    var loadWidth = window.innerWidth; // save window load width as a variable
+
+    $(window).resize(function() { // when the window is resized...
+
+        if ( loadWidth !== window.innerWidth ) { // trigger for width only...
+
+            if (window.innerWidth < 801) { // if width is less than 801px...
+
+                $('#navigation').hide(); // hide main navigation items
+
+            } else  { // else ...
+
+                $('#navigation').show(); // show main navigation items
                 $('ul.sub-menu').hide(); // hide sub navigation items
-				
-			} else  { // else ...
-			
-				$('#navigation').show(); // show main navigation items
-			}
-			
-		} // end trigger for width only
-		
-	}); // end window resize
+            }
+
+        } // end trigger for width only
+
+    }); // end window resize
 
 }); // end window load
